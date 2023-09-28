79879c2598290b791ccfbd5b49d8e3dbd1bf1395
Deucalio <hjamad89@gmail.com>
2023-09-28 16:41:55 +0500

    add slider in clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,14 +1,157 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./clients.css";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const Clients = () => {
   return (
-    <section className="px-10 py-6">
+    <section className="px-4 py-6  border-rose-500">
       <p className="mx-auto w-2/3 pb-12 text-xl uppercase leading-8 tracking-tighter">
         Here's what our clients has shared with us
       </p>
 
-      <div className="mx-auto flex w-64 flex-col gap-5 px-2 py-4 md:grid md:w-full md:grid-cols-2 lg:grid-col">
-        <article className="flex h-96 flex-col gap-5 rounded-3xl border-sky-400 bg-[#1E1E1E] px-8 py-4 lg:h-64">
+      <div
+        className=" border-sky-400 mx-auto flex w-96 flex-col gap-5 px-2 py-4 md:grid md:w-full md:grid-cols-2 lg:grid-col
+       h-96 w-96 border-2 sm:border-2"
+      >
+        <Swiper
+          slidesPerView={2}
+          centeredSlides={true}
+          spaceBetween={30}
+          grabCursor={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper  w-full  lg:w-[75rem] h-full"
+        >
+          <SwiperSlide class="">
+            <article className="w-60 flex h-full flex-col gap-5 rounded-3xl border-sky-400 bg-[#1E1E1E] px-8 py-4">
+              <img
+                className="h-14 w-14"
+                src="https://img.icons8.com/3d-fluency/94/quote-left.png"
+                alt="quote-left"
+              />
+
+              <p className="text-xs leading-6 text-zinc-500">
+                I couldn't be more happier with the results that were provided
+                to me. It was everything that I anticipated from this platform!
+              </p>
+
+              <ul className="mt-8 grid h-20 grid-cols-2 grid-rows-2 -space-x-7 text-sm opacity-75">
+                <li className="col-span-1 row-span-2">
+                  <img
+                    className="h-12 w-12 rounded-full 
+            lg:-translate-y-7"
+                    src="https://images.generated.photos/-xGFYXL6ts6pLJa6UKFheSLpVvkmH6sSbmHL7WfYMos/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDA2MDc1LmpwZw.jpg"
+                    alt=""
+                  />
+                </li>
+                <li className=" lg:-translate-y-6">Marty McFly</li>
+                <li className=" -mt-2 text-zinc-400 lg:-translate-y-6">
+                  Data Scientist
+                </li>
+              </ul>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide className="w-60">
+            {/* <article className="w-60 flex h-full flex-col gap-5 rounded-3xl border-sky-400 bg-[#1B1C25] px-8 py-4">
+              <img
+                className="h-14 w-14"
+                src="https://img.icons8.com/3d-fluency/94/quote-left.png"
+                alt="quote-left"
+              />
+
+              <p className="text-xs leading-6 text-zinc-500 ">
+                I couldn't be more happier with the results that were provided
+                to me. It was everything that I anticipated from this platform!
+              </p>
+
+              <ul className="mt-8 grid h-20 grid-cols-2 grid-rows-2 -space-x-7 text-xs opacity-75 ">
+                <li className="col-span-1 row-span-2">
+                  <img
+                    className="h-12 w-12 rounded-full lg:-translate-y-7"
+                    src="https://images.generated.photos/ajBCQWS5wQiW_IWjBjm-D2-0yhxfhOk4VqRXKuaxUI8/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTIzMDY5LmpwZw.jpg"
+                    alt=""
+                  />
+                </li>
+                <li className="lg:-translate-y-6">Jeff Hardy</li>
+                <li className="-mt-2 text-zinc-400 -translate-y-6">
+                  Keyboard warrior
+                </li>
+              </ul>
+            </article> */}
+             <article className="w-60 flex h-full flex-col gap-5 rounded-3xl border-sky-400 bg-[#1E1E1E] px-8 py-4">
+              <img
+                className="h-14 w-14"
+                src="https://img.icons8.com/3d-fluency/94/quote-left.png"
+                alt="quote-left"
+              />
+
+              <p className="text-xs leading-6 text-zinc-500">
+                I couldn't be more happier with the results that were provided
+                to me. It was everything that I anticipated from this platform!
+              </p>
+
+              <ul className="mt-8 grid h-20 grid-cols-2 grid-rows-2 -space-x-7 text-sm opacity-75">
+                <li className="col-span-1 row-span-2">
+                  <img
+                    className="h-12 w-12 rounded-full 
+            lg:-translate-y-7"
+                    src="https://images.generated.photos/-xGFYXL6ts6pLJa6UKFheSLpVvkmH6sSbmHL7WfYMos/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDA2MDc1LmpwZw.jpg"
+                    alt=""
+                  />
+                </li>
+                <li className=" lg:-translate-y-6">Marty McFly</li>
+                <li className=" -mt-2 text-zinc-400 lg:-translate-y-6">
+                  Data Scientist
+                </li>
+              </ul>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide>
+            <article className="w-60 flex h-full flex-col gap-5 rounded-3xl border-sky-400 bg-[#1B1C25] px-8 py-4">
+              <img
+                className="h-14 w-14"
+                src="https://img.icons8.com/3d-fluency/94/quote-left.png"
+                alt="quote-left"
+              />
+
+              <p className="text-xs leading-6 text-zinc-500 ">
+                I couldn't be more happier with the results that were provided
+                to me. It was everything that I anticipated from this platform!
+              </p>
+
+              <ul className="mt-8 grid h-20 grid-cols-2 grid-rows-2 -space-x-7 text-xs opacity-75 ">
+                <li className="col-span-1 row-span-2">
+                  <img
+                    className="h-12 w-12 rounded-full lg:-translate-y-7"
+                    src="https://images.generated.photos/ajBCQWS5wQiW_IWjBjm-D2-0yhxfhOk4VqRXKuaxUI8/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTIzMDY5LmpwZw.jpg"
+                    alt=""
+                  />
+                </li>
+                <li className="lg:-translate-y-6">Jeff Hardy</li>
+                <li className="-mt-2 text-zinc-400 -translate-y-6">
+                  Keyboard warrior
+                </li>
+              </ul>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+        {/* <article className="flex h-96 flex-col gap-5 rounded-3xl border-sky-400 bg-[#1E1E1E] px-8 py-4 lg:h-64">
           <img
             className="h-14 w-14"
             src="https://img.icons8.com/3d-fluency/94/quote-left.png"
@@ -88,7 +231,7 @@ const Clients = () => {
               Emoji specialist
             </li>
           </ul>
-        </article>
+        </article> */}
       </div>
     </section>
   );
